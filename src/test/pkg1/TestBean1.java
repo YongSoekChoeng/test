9bb6361ba71c4a8ede5e7f6773ad0655cba376e7
jysn007 <jysn007@hanmail.net>
2024-11-07 20:42:50 +0900

    .

--- src/test/pkg1/TestBean1.java
+++ src/test/pkg1/TestBean1.java
@@ -3,7 +3,7 @@ package test.pkg1;
 public class TestBean1 {
 
 	public void test1(String name) {
-		System.out.println("name["+name+"]");
+		System.out.println("name["+name+"]2");
 	}
 	
 	public void test2(String name) {
