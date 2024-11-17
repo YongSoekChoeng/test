ebb396fd508d07055127fb87d115b84b1cb67f73
YongSoekChoeng <jysn007@gmail.com>
2024-11-17 11:00:36 +0900

    .

--- src/test/pkg1/TestBean1.java
+++ src/test/pkg1/TestBean1.java
@@ -3,7 +3,7 @@ package test.pkg1;
 public class TestBean1 { 
 
 	public void test1(String name) {
-		System.out.println("name["+name+"]222");
+		System.out.println("name["+name+"]");
 	}
 	
 	public void test2(String name) {
